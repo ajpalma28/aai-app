8573a8281a7befbcb9bad438d0f77651d3898b86
ajpalma28 <46094280+ajpalma28@users.noreply.github.com>
2022-07-09 13:59:10 +0200

    #7 Created BLE3
    Nuevo proyecto para seguir realizando pruebas en la programación de apps con conexión con dispositivos BLE.

---
+++ code/example/BLE3/app/src/main/java/com/example/ble3/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.ble3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// Se va a basar en https://www.youtube.com/watch?v=Wv4WrPMsx24
+// LO DEJO APROXIMADAMENTE EN 5:13
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
