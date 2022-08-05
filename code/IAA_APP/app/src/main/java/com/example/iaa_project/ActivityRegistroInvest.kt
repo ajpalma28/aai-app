49305f2ff42a4989d8c6a85c2e6b7f4ffab23f09
ajpalma28 <46094280+ajpalma28@users.noreply.github.com>
2022-08-05 17:56:28 +0200

    #26 Update project
    He avanzado en el proyecto, pero da error al intentar conectarse con la base de datos. ¡¡Intentar solucionarlo!!

--- code/IAA_APP/app/src/main/java/com/example/iaa_project/ActivityRegistroInvest.kt
+++ code/IAA_APP/app/src/main/java/com/example/iaa_project/ActivityRegistroInvest.kt
@@ -1,11 +1,12 @@
 package com.example.iaa_project
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.iaa_project.databinding.ActivityRegistroInvestBinding
-import com.example.iaa_project.databinding.ActivityRegistroInvestBinding.*
+import com.example.iaa_project.databinding.ActivityRegistroInvestBinding.inflate
+import java.sql.DriverManager
+import java.sql.Connection
 
 class ActivityRegistroInvest : AppCompatActivity() {
     var variables: ActivityRegistroInvestBinding? = null
@@ -15,15 +16,111 @@ class ActivityRegistroInvest : AppCompatActivity() {
 
         variables = inflate(layoutInflater)
         setContentView(variables!!.root)
+
+        val botonAceptar = variables!!.btnAceptReg
+
+        botonAceptar.setOnClickListener{
+            registerInvest()
+        }
+
     }
 
-    fun registerInvest(view: View) {
-        val entradaDNI = variables!!.editInvestDNI
-        val entradaNombre = variables!!.editInvestName
-        val entradaApellidos = variables!!.editInvestApell
-        val entradaFecha = variables!!.editInvestNac
-        val entradaPW1 = variables!!.editInvestContr
-        val entradaPW2 = variables!!.editInvestConfContr
+    fun registerInvest() {
+        val entradaDNI = variables!!.editInvestDNI.text
+        val entradaNombre = variables!!.editInvestName.text
+        val entradaApellidos = variables!!.editInvestApell.text
+        val entradaFecha = variables!!.editInvestNac.text
+        val entradaPW1 = variables!!.editInvestContr.text
+        val entradaPW2 = variables!!.editInvestConfContr.text
         val entradaTC = variables!!.checkTerminos
+
+        val dni = entradaDNI.toString()
+        val nombre = entradaNombre.toString()
+        val apellidos = entradaApellidos.toString()
+        val fecha = entradaFecha.toString()
+        val pw1 = entradaPW1.toString()
+        val pw2 = entradaPW2.toString()
+        val term = entradaTC.isChecked
+        var contra: String? = null
+
+        val id = generaIdInvestigador(nombre, apellidos, dni, fecha)
+        var tyc: String = "false"
+        if(term){
+            tyc = "true"
+        }
+
+        val fechaDef = formateaFecha(fecha)
+
+        if(pw1==pw2){
+            contra = pw1
+        }
+
+        try{
+            println("Entro en el try")
+            //Class.forName("com.mysql.jdbc.Driver")
+            //Configuracion de la conexión
+            //Configuracion de la conexión
+            println("Hola 1")
+            val connection = DriverManager.getConnection(
+                "jdbc:mysql://127.0.0.1:3306/aai-app_local",
+                "Admin",
+                "root"
+            )
+            println(connection.isValid(0))
+            println("hola 2")
+
+
+            val statement = connection.createStatement()
+            println("Voy a la query")
+            //Guardo en resultSet el resultado de la consulta
+            //Guardo en resultSet el resultado de la consulta
+            //val resultSet =
+                            //statement.executeQuery("select * from usuarios where usuario = '$resUsuario' and pass = '$resPassword'")
+            //"INSERT INTO `db-tfg`.`investigador` (`idinvestigador`, `dni`, `apellidos`, `nombre`, `fnacimiento`, `contrasena`, `notificaciones`, `terminoscondiciones`) VALUES ('$id', '$dni', '$apellidos', '$nombre', '$fecha', '$contra', 'true', '$tyc');"
+            val resultSet =
+                statement.executeQuery("\"INSERT INTO `db-tfg`.`investigador` (`idinvestigador`, `dni`, `apellidos`, `nombre`, `fnacimiento`, `contrasena`, `notificaciones`, `terminoscondiciones`) VALUES ('$id', '$dni', '$apellidos', '$nombre', '$fechaDef', '$contra', 'true', '$tyc');")
+
+            Toast.makeText(this,"¡REGISTRADO CORRECTAMENTE! Su identificador es $id", Toast.LENGTH_LONG).show()
+
+        } catch (e: Exception) {
+            println(e.toString())
+        }
+
     }
+
+    private fun generaIdInvestigador(nombre: String, apellidos: String, dni: String, fecha: String){
+        var identificador = ""
+        for(i in nombre.indices){
+            if(i<4){
+                identificador += nombre[i]
+            }else{
+                break
+            }
+        }
+        for(j in dni.indices){
+            if(j>5){
+                identificador += dni[j]
+            }
+        }
+        for(i in apellidos.indices){
+            if(i<4){
+                identificador += apellidos[i]
+            }else{
+                break
+            }
+        }
+        var partes = fecha.split("/")
+        var ano = partes[2]
+        for(j in ano.indices){
+            if(j>1){
+                identificador += ano[j]
+            }
+        }
+    }
+
+    private fun formateaFecha(fecha: String): String {
+        var partes = fecha.split("/")
+        return partes[2]+"-"+partes[1]+"-"+partes[0]
+    }
+
 }
--- code/IAA_APP/app/src/main/java/com/example/iaa_project/MainActivity.kt
+++ code/IAA_APP/app/src/main/java/com/example/iaa_project/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.iaa_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.iaa_project.databinding.ActivityMainBinding
+import com.example.iaa_project.databinding.ActivityRegistroInvestBinding
+import com.example.iaa_project.databinding.ActivityRegistroInvestBinding.*
 
 class MainActivity : AppCompatActivity() {
+    var variables: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val variables = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(variables.root)
+
+        val botonReg = variables.btnRegistro
+        val botonInSes = variables.btnInicioSesion
+        val botonAcercaDe = variables.btnAcercaDe
+
+        botonReg.setOnClickListener{
+            val intento1 = Intent(this, ActivityRegistroInvest::class.java)
+            startActivity(intento1)
+        }
     }
 }
